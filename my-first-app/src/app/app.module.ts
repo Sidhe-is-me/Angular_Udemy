7e3e4a2dc8896638d94985204f2f5e7f2a1589a6
Yvonne Dreptate <YvonneDreptate@gmail.com>
2019-04-25 10:48:22 -0400

    thru lesson 20

--- my-first-app/src/app/app.module.ts
+++ my-first-app/src/app/app.module.ts
@@ -1,14 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+// import { FormsModule } from '@angular/core';
+// import { HttpModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { AngularWaitBarrier } from 'blocking-proxy/built/lib/angular_wait_barrier';
+import { ServerComponent } from './server/server.component';
+import { from } from 'rxjs';
+import { ServersComponent } from './servers/servers.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ServerComponent,
+    ServersComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    // FormsModule,
+    // HttpModule
   ],
   providers: [],
   bootstrap: [AppComponent]
